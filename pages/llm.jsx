c0109ae3924d3bf607a1c06c1d84208f22489aa9
Aryan <67077646+AryanPadmanabhan@users.noreply.github.com>
2023-07-13 15:09:10 -0500

    Update llm.jsx

--- pages/llm.jsx
+++ pages/llm.jsx
@@ -4,7 +4,7 @@ import ml from '../public/assets/ml.png'
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
-const crypto = () => {
+const llm = () => {
   return (
     <div className='w-full dark:bg-gray-800'>
       <div className='w-screen h-[50vh] relative'>
@@ -17,44 +17,15 @@ const crypto = () => {
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>LLM POWERED Q&A</h2>
+          <h2 className='py-2'>
+          LLM POWERED Q&A</h2>
           <h3>Python / Pinecone</h3>
         </div>
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[90%] right-[50%] translate-x-[-10%] translate-y-[-70%] text-white z-10 mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4 text-white bg-black/50'>
-          <div className='p-2'>
-            <p className='text-center font-bold pb-2 '>Technologies</p>
-            <div className='grid grid-cols-3 md:grid-cols-1 '>
-              <p className='py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> ChatGPT
-              </p>
-              <p className='py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Python
-              </p>
-              <p className='py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Pandas
-              </p>
-              <p className='py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Numpy
-              </p>
-              <p className='py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Pinecone
-              </p>
-              <p className='py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> APIs
-              </p>
-            </div>
-          </div>
-        </div>
-        </div>
-
-
       </div>
 
-      <div className='max-w-[1240px] mx-auto p-2 gap-8 py-8'>
-        <div className='col-span-4 text-white'>
-          {/* <p>Project</p> */}
-          <h1>Overview</h1>
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8 text-white'>
+        <div className='col-span-4'>
+        <h1>Overview</h1>
           <p className='pb-8'>
             With the rise of Transformer based neural networks to constuct complex Large Language Models (LLMs), it's easy to see how incredible these powerful architectures are.
             ChatGPT has been fine-tuned for the purpose to answer questions, so I took advantage of this in order to introduce my own sets of information for information extraction. 
@@ -65,14 +36,38 @@ const crypto = () => {
             LLMs to answer the question (query) with only the information provided from the vectors. 
           </p>
 
-
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4 text-white'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1'>
+              <p className='text-white py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> ChatGPT
+              </p>
+              <p className='text-white py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Python
+              </p>
+              <p className='text-white py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Pandas
+              </p>
+              <p className='text-white py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Numpy
+              </p>
+              <p className='text-white py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Pinecone
+              </p>
+              <p className='text-white py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> APIs
+              </p>
+            </div>
+          </div>
         </div>
         <Link href='/#projects'>
-          <p className='underline cursor-pointer pb-20'>Back</p>
+          <p className='underline cursor-pointer'>Back</p>
         </Link>
       </div>
     </div>
   );
 };
 
-export default crypto;
+export default llm;
